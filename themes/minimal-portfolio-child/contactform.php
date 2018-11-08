c00184c726f6d6f9a69782995b3fbb66a384453c
Jess O'Donnell <jessodonnell23@gmail.com>
2018-11-08 18:34:49 -0500

    contact form part 2
    This file is the response funtion of my contact form.

---
+++ themes/minimal-portfolio-child/contactform.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  
+<body>
+  
+  Welcome <?php echo $_POST["name"];?><br>
+  Your E-mail address is: <?php echo $_POST["email"];?><br>
+  Your Subject is: <?php echo $POST["subject"]?><br>
+  Your Message is: <?php echo $POST["message"]?>
+
+</body>
+  
+</html>
