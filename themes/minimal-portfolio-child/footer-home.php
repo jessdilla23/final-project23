e374a63ac8aca7fcd3a2269742d7b8b277844aab
Jess O'Donnell <jessodonnell23@gmail.com>
2018-11-08 18:34:20 -0500

    contact form
    This php file represents my contact form appearing at the bottom of my site.

---
+++ themes/minimal-portfolio-child/footer-home.php
@@ -0,0 +1,10 @@
+
+<p1> Contact Me </p1>
+
+<form action="contactform.php" method="post">
+	Name: <input type="text" name="name"><br>
+	E-mail: <input type="text" name="email"><br>
+	Subject: <input type="text" name="subject"><br>
+	Message: <textarea name="message" rows="5" cols="40"></textarea><br>
+<input type="submit">
+</form>	
