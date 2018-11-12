bd4a87e8fd59df7e66f230558640fa696c876e63
Jess O'Donnell <jessodonnell23@gmail.com>
2018-11-11 23:29:24 -0500

    social media icons
    Added social media icons to my footer. They're finally centered!

--- themes/minimal-portfolio-child/footer.php
+++ themes/minimal-portfolio-child/footer.php
@@ -28,6 +28,16 @@
 					<div class="col-md-12">
 						<ul class="site-info">
 							
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+	<a href="https://www.linkedin.com/in/jessodonnell23/" class="fa fa-linkedin"></a>
+	<a href="https://www.instagram.com/holyjesssus/" class="fa fa-instagram"></a>
+	<a href="https://twitter.com/holyjesssus" class="fa fa-twitter"></a><br>
+
+	<br>
+
+	<p4>Copyright 2017 © Jessica O'Donnell </p4>
 
 	</footer><!-- #colophon -->
 
